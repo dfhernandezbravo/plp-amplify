0d1c7d3d357e2469f0d6fc8751ff7f9b279aa488
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2023-06-07 15:20:34 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+const PdpLayout = () => {
+  return (
+     
+        <div>
+          <h1> PLP remote Here ! </h1>
+        </div>
+     
+  );
+};
+export default PdpLayout;
