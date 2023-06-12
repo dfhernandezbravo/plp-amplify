a094947d07ccaabdcacbcdda112a1a537d0834b9
Jose Martinez <jose.martinezmayora@externos-cl.cencosud.com>
2023-06-12 09:14:29 -0400

    adding component and module structure

---
+++ src/presentation/module/ProductListPage/layouts/desktop/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Desktop from "@/presentation/components/layout/desktop";
+import ProductContent from '../../section/ProductContent';
+
+function ProductListPageDesktop() {
+    return (
+        <Desktop>
+          <ProductContent />
+        </Desktop>
+    )
+}
+
+export default ProductListPageDesktop
---
+++ src/presentation/module/ProductListPage/section/ProductContent/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductContent() {
+  return (
+    <div>ProductContent</div>
+  )
+}
+
+export default ProductContent
