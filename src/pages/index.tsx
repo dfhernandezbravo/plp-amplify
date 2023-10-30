02a55270f0b532c03e2cf5d11faa1fc1973e390c
Victor Mansilla <victor.mansillamunoz@cencosud.cl>
2023-10-30 12:01:05 -0300

    fix: remove component in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,7 @@
 import * as React from 'react';
 
-// Components
-import Breadcrumb from '@components/molecules/Breadcrumb';
-
 const App = () => {
-  return (
-    <Breadcrumb
-      links={[
-        {
-          url: '/muebles',
-          text: 'Muebles',
-        },
-        {
-          url: '/muebles-living',
-          text: 'Muebles de Living y Sala de Estar',
-        },
-        {
-          url: '/juegos-living',
-          text: 'Juegos de Living',
-        },
-      ]}
-    />
-  );
+  return <></>;
 };
 
 export default App;
