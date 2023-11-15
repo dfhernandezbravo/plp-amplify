3a607ec8a6968ddbdef3b930c42b7ed08ef82e6c
Victor Mansilla <victor.mansillamunoz@cencosud.cl>
2023-11-15 11:46:10 -0300

    fix: clean index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,7 @@
 import * as React from 'react';
 
-// Components
-import Banner, { Item } from '@components/organisms/Banner';
-
 const App = () => {
-  // Constants
-  const items: Item[] = [
-    {
-      url: 'https://www.easy.cl/eventos/clima',
-      image:
-        'https://media.easy.cl/is/image/EasySA/bannerplp_modofresh_desktop1',
-      title: 'Promo 1',
-      external: true,
-    },
-    {
-      url: 'https://www.easy.cl/eventos/clima',
-      image:
-        'https://media.easy.cl/is/image/EasySA/bannerplp_modofresh_desktop1',
-      title: 'Promo 2',
-      external: true,
-    },
-  ];
-
-  return (
-    <Banner
-      pagination
-      items={items}
-      autoplay={{ delay: 5000 }}
-      onClick={(item) => console.log(item)}
-    />
-  );
+  return <></>;
 };
 
 export default App;
