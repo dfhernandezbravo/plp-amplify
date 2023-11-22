dc72f588436f70d19fc79e5a544e231aa2bad8dd
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2023-11-22 18:37:05 +0000

    feat: install Easy DS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+// import '../presentation/styles/global.css';
 
 import './style.css';
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import {
+  ButtonEasy,
+  ThemeProviderEasy,
+  useDevice,
+} from '@cencosud-ds/easy-design-system';
 
 const App = () => {
-  return <></>;
+  const { device, os } = useDevice();
+  return (
+    <ThemeProviderEasy>
+      <h3>Device: {device}</h3>
+      <h3>OS: {os}</h3>
+      <ButtonEasy variant="primary" label="Button Test" />
+    </ThemeProviderEasy>
+  );
 };
 
 export default App;
