4b4846511881b0c8123cd0763a958d1c98d53cdd
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-01-31 09:48:44 -0500

    refactor: select component from bit lib

--- src/presentation/components/molecules/Filter/Filter.tsx
+++ src/presentation/components/molecules/Filter/Filter.tsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from 'react';
-
-// Components
 import Icon from '@components/atoms/Icon';
 import Select, { Option } from '@components/atoms/Select';
-
-// Styled components
-import { Container, Text } from './styles';
-
-// Definitions
-import { Props, OrderOptions } from './types';
-
-// Constants
+import { useEffect, useState } from 'react';
 import { options } from './constants';
+import { Container, Text } from './styles';
+import { OrderOptions, Props } from './types';
 
 const Filter = (props: Props) => {
-  // Props
   const { onChange, defaultValue = '' } = props;
-
-  // State
   const [key, setKey] = useState(0);
   const [currentOption, setCurrentOption] = useState<Option>();
 
-  // Methods
   const onOrderChange = (option: Option) => {
     const order = option.value as OrderOptions;
     if (order) onChange?.(order);
     else onChange?.(null as unknown as OrderOptions);
   };
+
   const calculateDefaultOption = () => {
     const defaultOption = options.find(
       (option) => option.value === defaultValue,
@@ -37,7 +26,6 @@ const Filter = (props: Props) => {
     }
   };
 
-  // Effects
   useEffect(() => {
     calculateDefaultOption();
   }, [defaultValue]);
@@ -48,12 +36,9 @@ const Filter = (props: Props) => {
       <Text>Ordenar por:</Text>
       <Select
         key={key}
-        id="select-order"
-        name="select-order"
-        className="select-order"
         options={options}
         onChange={onOrderChange}
-        defaultValue={currentOption}
+        value={currentOption}
       />
     </Container>
   );
--- src/presentation/components/molecules/Filter/constants.ts
+++ src/presentation/components/molecules/Filter/constants.ts
@@ -1,7 +1,7 @@
-import { Option } from '@components/atoms/Select';
+import { OptionsSelect } from '@ccom-easy-design-system/atoms.select/dist/types';
 
 // TODO: Agregar esto a variables de dominio
-export const options: Option[] = [
+export const options: OptionsSelect[] = [
   { label: 'Destacados', value: 'orders:desc' },
   { label: 'Precios más bajos', value: 'price:asc' },
   { label: 'Precios más altos', value: 'price:desc' },
