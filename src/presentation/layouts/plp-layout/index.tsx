f54ce1aea468b88376840d75d4be4feef280dbfa
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-03-25 14:09:13 -0500

    feat: add events to get device and dimensions in PLPLayout

--- src/presentation/layouts/plp-layout/index.tsx
+++ src/presentation/layouts/plp-layout/index.tsx
@@ -2,13 +2,31 @@ import ThemeProvider from '@components/atoms/theme-provider';
 import QueryProvider from '@presentation/providers/query-provider';
 import PlpProvider from '@store/plp-provider';
 import StoreProvider from '@store/provider';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const PLPLayout: React.FC<Props> = ({ children }) => {
+  useEffect(() => {
+    document.addEventListener('GET_DEVICE_AND_DIMENSIONS', (event) => {
+      event.stopPropagation();
+      console.log('PLP:', { event });
+    });
+
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    document.dispatchEvent(
+      new CustomEvent('DISPATCH_GET_DEVICE_AND_DIMENSIONS', { detail: {} }),
+    );
+    window.dispatchEvent(
+      new CustomEvent('DISPATCH_GET_DEVICE_AND_DIMENSIONS', { detail: {} }),
+    );
+  }, []);
+
   return (
     <QueryProvider>
       <StoreProvider>
