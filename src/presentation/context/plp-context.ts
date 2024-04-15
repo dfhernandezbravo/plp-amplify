0e5135c4374911d726adcf186d4b6e5dba109b03
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-04-15 10:16:06 -0500

    refactor: :recycle: create plp context and product entity

---
+++ src/presentation/context/plp-context.ts
@@ -0,0 +1,24 @@
+import { Content } from '@entities/cms';
+import { Facets } from '@entities/product/facets.entity';
+import { Product } from '@entities/product/product.entity';
+import { createContext } from 'react';
+
+interface PLPContextProps {
+  isLoadingProducts: boolean;
+  isLoadingCMS: boolean;
+  products: Product[];
+  facets: Facets[];
+  contentCMS: Content[];
+  refreshProducts: () => void;
+}
+
+const PLPContext = createContext<PLPContextProps>({
+  isLoadingCMS: false,
+  isLoadingProducts: false,
+  products: [],
+  facets: [],
+  contentCMS: [],
+  refreshProducts: () => {},
+});
+
+export default PLPContext;
