cf0c0740e24d45c1d5a93c2a4a8fb01ec8009c8f
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-04-15 15:46:06 -0500

    perf(components): :recycle: create layout component

--- src/presentation/modules/plp-standard/components/facets/index.tsx
+++ src/presentation/modules/plp-standard/components/facets/index.tsx
@@ -1,7 +1,7 @@
-import { Layout } from '@cencosud-ds/easy-design-system';
+import { Layout } from '@components/atoms/layout';
+import { useAppSelector } from '@store/hooks';
 import FacetsDesktop from './layout/desktop';
 import FacetsMobile from './layout/mobile';
-import { useAppSelector } from '@store/hooks';
 
 const Facets = () => {
   const { isOpenFacetsMobile } = useAppSelector((state) => state.products);
