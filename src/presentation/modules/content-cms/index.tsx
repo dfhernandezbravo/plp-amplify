83981bb90eb8ab44728c9ec59d006863acd02e19
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2024-05-17 00:22:15 -0300

    feat: plp skeleton transition

--- src/presentation/modules/content-cms/index.tsx
+++ src/presentation/modules/content-cms/index.tsx
@@ -2,12 +2,9 @@ import ComposerCMS from '@components/organisms/composer-cms';
 import PLPContext from '@presentation/context/plp-context';
 import { useContext } from 'react';
 import ContentComponent from './components';
-import CMSSkeleton from './components/skeleton';
 
 const ContentCMS = () => {
-  const { isLoadingCMS, contentCMS } = useContext(PLPContext);
-
-  if (isLoadingCMS) return <CMSSkeleton />;
+  const { contentCMS } = useContext(PLPContext);
 
   if (!contentCMS) return null;
 
